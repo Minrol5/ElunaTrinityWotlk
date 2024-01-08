b33ea15812c64f611dc779b5ab14cf18f6ad02de
Minrol5 <idi923@yahoo.de>
2024-01-08 07:47:03 -0800

    Test

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -16,9 +16,11 @@
  */
 
 // This is where scripts' loading functions should be declared:
+void AddSC_custom_reload_commands();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
+    AddSC_custom_reload_commands();
 }
